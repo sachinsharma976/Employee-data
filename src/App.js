5cfab8f4b7d60b49d46207ccac1cc719d50502f0
Sachin976 <sachinshoff@gmail.com>
2022-09-04 21:38:17 +0530

    mock data added

--- src/App.js
+++ src/App.js
@@ -15,9 +15,13 @@ function App() {
     showModal:false,
     modalState:'',
     employees:[
-      {id: '80d56852-8776-4e5e-9fc9-75f41ef4e086', name: 'sachin', department: 'frontend', salary: '1234'},
-      {id: '31dc96eb-c023-420f-8b8f-0ce89ed8df45', name: 'saurabh', department: 'backend', salary: '1234'},
-      {id: '94704858-0dd6-4ed8-9b50-0948ed939e29', name: 'avnish', department: 'frontend', salary: '1212'}
+      {name: 'fredie', department: 'frontend', salary: '5293293', id: '60421471-1bdc-4228-8c5a-01a2338df925'},
+      {name: 'piyush yadav', department: 'backend', salary: '276347', id: '0e61b832-61ef-4d94-816e-2b52e848f9f7'},
+      {name: 'sagar sharma', department: 'backend', salary: '127382', id: '70aa8f27-1f42-4bb7-a292-50d160820443'},
+      {name: 'satyam', department: 'backend', salary: '99999', id: '356c78df-5c35-4d55-901c-6bb9929138b1'},
+      {name: 'karan', department: 'backend', salary: '67442', id: '3b95a65d-d14c-41b3-8b82-6a6e5dd6e861'},
+      {name: 'sachin sharma', department: 'frontend', salary: '1234', id: '80d56852-8776-4e5e-9fc9-75f41ef4e086'},
+      {id: '94704858-0dd6-4ed8-9b50-0948ed939e29', name: 'avnish', department: 'frontend', salary: '1212'},
     ]
   }
   const filterInitialState = {
